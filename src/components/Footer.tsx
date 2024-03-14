7eecbcfb981d956c301a93c54b7fefdb24b13b7d
buivantoana <106064775+buivantoana@users.noreply.github.com>
2024-03-14 21:36:28 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,37 +4,189 @@ type Props = {};
 
 const Footer = () => {
   return (
-    <footer className='bg-white rounded-lg shadow m-4 dark:bg-gray-800'>
-      <div className='w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between'>
-        <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
-          © 2023{" "}
-          <a href='https://flowbite.com/' className='hover:underline'>
-            Flowbite™
-          </a>
-          . All Rights Reserved.
-        </span>
-        <ul className='flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0'>
-          <li>
-            <a href='#' className='hover:underline me-4 md:me-6'>
-              About
-            </a>
-          </li>
-          <li>
-            <a href='#' className='hover:underline me-4 md:me-6'>
-              Privacy Policy
-            </a>
-          </li>
-          <li>
-            <a href='#' className='hover:underline me-4 md:me-6'>
-              Licensing
-            </a>
-          </li>
-          <li>
-            <a href='#' className='hover:underline'>
-              Contact
-            </a>
-          </li>
-        </ul>
+    <footer className="bg-dark" id="tempaltemo_footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 pt-5">
+            <h2 className="h2 text-success border-bottom pb-3 border-light logo">
+              Zay Shop
+            </h2>
+            <ul className="list-unstyled text-light footer-link-list">
+              <li>
+                <i className="fas fa-map-marker-alt fa-fw" />
+                123 Consectetur at ligula 10660
+              </li>
+              <li>
+                <i className="fa fa-phone fa-fw" />
+                <a className="text-decoration-none" href="tel:010-020-0340">
+                  010-020-0340
+                </a>
+              </li>
+              <li>
+                <i className="fa fa-envelope fa-fw" />
+                <a
+                  className="text-decoration-none"
+                  href="mailto:info@company.com"
+                >
+                  info@company.com
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-4 pt-5">
+            <h2 className="h2 text-light border-bottom pb-3 border-light">
+              Products
+            </h2>
+            <ul className="list-unstyled text-light footer-link-list">
+              <li>
+                <a className="text-decoration-none" href="#">
+                  Luxury
+                </a>
+              </li>
+              <li>
+                <a className="text-decoration-none" href="#">
+                  Sport Wear
+                </a>
+              </li>
+              <li>
+                <a className="text-decoration-none" href="#">
+                  Men's Shoes
+                </a>
+              </li>
+              <li>
+                <a className="text-decoration-none" href="#">
+                  Women's Shoes
+                </a>
+              </li>
+              <li>
+                <a className="text-decoration-none" href="#">
+                  Popular Dress
+                </a>
+              </li>
+              <li>
+                <a className="text-decoration-none" href="#">
+                  Gym Accessories
+                </a>
+              </li>
+              <li>
+                <a className="text-decoration-none" href="#">
+                  Sport Shoes
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-4 pt-5">
+            <h2 className="h2 text-light border-bottom pb-3 border-light">
+              Further Info
+            </h2>
+            <ul className="list-unstyled text-light footer-link-list">
+              <li>
+                <a className="text-decoration-none" href="#">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a className="text-decoration-none" href="#">
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a className="text-decoration-none" href="#">
+                  Shop Locations
+                </a>
+              </li>
+              <li>
+                <a className="text-decoration-none" href="#">
+                  FAQs
+                </a>
+              </li>
+              <li>
+                <a className="text-decoration-none" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="row text-light mb-4">
+          <div className="col-12 mb-3">
+            <div className="w-100 my-3 border-top border-light" />
+          </div>
+          <div className="col-auto me-auto">
+            <ul className="list-inline text-left footer-icons">
+              <li className="list-inline-item border border-light rounded-circle text-center">
+                <a
+                  className="text-light text-decoration-none"
+                  target="_blank"
+                  href="http://facebook.com/"
+                >
+                  <i className="fab fa-facebook-f fa-lg fa-fw" />
+                </a>
+              </li>
+              <li className="list-inline-item border border-light rounded-circle text-center">
+                <a
+                  className="text-light text-decoration-none"
+                  target="_blank"
+                  href="https://www.instagram.com/"
+                >
+                  <i className="fab fa-instagram fa-lg fa-fw" />
+                </a>
+              </li>
+              <li className="list-inline-item border border-light rounded-circle text-center">
+                <a
+                  className="text-light text-decoration-none"
+                  target="_blank"
+                  href="https://twitter.com/"
+                >
+                  <i className="fab fa-twitter fa-lg fa-fw" />
+                </a>
+              </li>
+              <li className="list-inline-item border border-light rounded-circle text-center">
+                <a
+                  className="text-light text-decoration-none"
+                  target="_blank"
+                  href="https://www.linkedin.com/"
+                >
+                  <i className="fab fa-linkedin fa-lg fa-fw" />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-auto">
+            <label className="sr-only" htmlFor="subscribeEmail">
+              Email address
+            </label>
+            <div className="input-group mb-2">
+              <input
+                type="text"
+                className="form-control bg-dark border-light"
+                id="subscribeEmail"
+                placeholder="Email address"
+              />
+              <div className="input-group-text btn-success text-light">
+                Subscribe
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-100 bg-black py-3">
+        <div className="container">
+          <div className="row pt-2">
+            <div className="col-12">
+              <p className="text-left text-light">
+                Copyright © 2021 Company Name | Designed by{" "}
+                <a
+                  rel="sponsored"
+                  href="https://templatemo.com"
+                  target="_blank"
+                >
+                  TemplateMo
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </footer>
   );
